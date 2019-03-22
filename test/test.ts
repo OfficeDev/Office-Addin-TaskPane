5145e439882975318b17aa0ff686c57e51f78d45
Courtney Owen <cowen@microsoft.com>
2019-03-22 11:50:07 -0700

    Add test infra packages and add test framework for running on multiple hosts

--- test/test.ts
+++ test/test.ts
@@ -1,50 +1,59 @@
 import * as assert from "assert";
+import * as fs from "fs";
 import * as mocha from "mocha";
+const testJsonFile: string = `${__dirname}/testData.json`;
+const testJsonData = JSON.parse(fs.readFileSync(testJsonFile).toString());
 import * as testHelper from "office-addin-test-helpers"
 import * as testServerInfra from "office-addin-test-server";
-const yellowColor: string = "#FFFF00";
 const port: number = 8080;
 const testServer = new testServerInfra.TestServer(port);
 let testValues : any = [];
 
-describe("Setup test environment", function () {
-    describe("Start sideload, start dev-server, and start test-server", function () {
-        it("Sideload should have completed and dev-server should have started", async function () {
-            this.timeout(0);
-            const startDevServer = await testHelper.startDevServer();
-            const sideloadApplication = await testHelper.sideloadDesktopApp("excel", "test/test-manifest.xml");
-            assert.equal(startDevServer, true);
-            assert.equal(sideloadApplication, true);
-        });
-        it("Test server should have started and Excel should have pinged the server", async function () {
-            this.timeout(0);
-            const testServerStarted = await testServer.startTestServer();
-            assert.equal(testServerStarted, true);
-        });
-    });
- });
+Object.keys(testJsonData.hosts).forEach(function (host) {
+    const expectedResult: string = testJsonData.hosts[host].expectedResult;
 
-describe("Test Taskpane Project", function () {
-    describe("Get test results for taskpane project", function () {
-        it("should get results from the taskpane application", async function () {
-            this.timeout(0);
-            testValues = await testServer.getTestResults();
-            assert.equal(testValues.length, 1);
-        });
-        it("Cell fill color should be yellow", async function () {
-            assert.equal(yellowColor, testValues[0].Value);
+    describe("Setup test environment", function () {
+        describe("Start sideload, start dev-server, and start test-server", function () {
+            it(`Sideload should have completed for ${host} and dev-server should have started`, async function () {
+                this.timeout(0);
+                const startDevServer = await testHelper.startDevServer();
+                const sideloadApplication = await testHelper.sideloadDesktopApp(host, "test/test-manifest.xml");
+                assert.equal(startDevServer, true);
+                assert.equal(sideloadApplication, true);
+            });
+            it(`Test server should have started and ${host} should have pinged the server`, async function () {
+                this.timeout(0);
+                const testServerStarted = await testServer.startTestServer();
+                assert.equal(testServerStarted, true);
+            });
         });
     });
+
+    describe("Test Taskpane Project", function () {
+        describe("Get test results for taskpane project", function () {
+            it("Validate expected result count", async function () {
+                this.timeout(0);
+                testValues = await testServer.getTestResults();
+                assert.equal(testValues.length > 0, true);
+            });
+            it("Validate expcted result", async function () {
+                assert.equal(testValues[0].Value, expectedResult);
+            });
+        });
+    });
+
+    describe("Teardown test environment", function () {
+        describe(`Kill ${host} and the test server`, function () {
+            it(`should close ${host} and stop the test server`, async function () {
+                this.timeout(10000);
+                const stopTestServer = await testServer.stopTestServer();
+                assert.equal(stopTestServer, true);
+                const testEnvironmentTornDown = await testHelper.teardownTestEnvironment(host)
+                assert.equal(testEnvironmentTornDown, true);
+            });
+        });
+    })
 });
 
-describe("Teardown test environment", function () {
-    describe("Kill Excel and the test server", function () {
-        it("should close Excel and stop the test server", async function () {
-            this.timeout(10000);
-            const stopTestServer = await testServer.stopTestServer();
-            assert.equal(stopTestServer, true);
-            const testEnvironmentTornDown = await testHelper.teardownTestEnvironment('excel');
-            assert.equal(testEnvironmentTornDown, true);
-        });
-    });
-})
+
+
