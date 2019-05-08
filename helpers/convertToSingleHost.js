a4b54c07b46f2d6249e1c6270b555ab84fedee66
Courtney Owen <cowen@microsoft.com>
2019-05-08 14:30:16 -0700

    Update convertToSingleHost.js

--- helpers/convertToSingleHost.js
+++ helpers/convertToSingleHost.js
@@ -1,9 +1,10 @@
-import * as util from "util";
+// import * as _ from 'lodash';
+// import * as util from "util";
 const fs = require('fs');
-const readFileAsync = util.promisify(fs.readFile);
-const unlinkFileAsync = util.promisify(fs.unlink);
-const writeFileAsync = util.promisify(fs.writeFile);
-const path = require('path');
+// const readFileAsync = util.promisify(fs.readFile);
+// const unlinkFileAsync = util.promisify(fs.unlink);
+// const writeFileAsync = util.promisify(fs.writeFile);
+// const path = require('path');
 const hosts = [
     "excel",
     "onenote",
@@ -13,103 +14,107 @@ const hosts = [
     "word"
 ];
 
-modifyProjectForSingleHost('c:/generator-office/foo', 'taskpane', 'excel', true);
+// delete all host specific files
+hosts.forEach(async function (host) {
+    await fs.unlinkFileAsync(`./../manifest.${host}.xml`);
+    await fs.unlinkFileAsync(`./../src/taskpane/${host}.${extension}`);
+});
 
-export function modifyProjectForSingleHost(projectFolder, projectType, host, typescript) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            await convertProjectToSingleHost(projectFolder, projectType, host, typescript);
-            await updatePackageJsonForSingleHost(projectFolder, host);
-            return resolve();
-        } catch (err) {
-            return reject(err);
-        }
-    });
-}
+// export function modifyProjectForSingleHost(projectFolder, projectType, host, typescript) {
+//     return new Promise(async (resolve, reject) => {
+//         try {
+//             await convertProjectToSingleHost(projectFolder, projectType, host, typescript);
+//             await updatePackageJsonForSingleHost(projectFolder, host);
+//             return resolve();
+//         } catch (err) {
+//             return reject(err);
+//         }
+//     });
+// }
 
-async function convertProjectToSingleHost(projectFolder, projectType, host, typescript) {
-    try {
-        let extension = typescript ? "ts" : "js";
-        // copy host-specific manifest over manifest.xml
-        const manifestContent = await readFileAsync(path.resolve(`${projectFolder}/manifest.${host}.xml`), 'utf8');
-        await writeFileAsync(path.resolve(`${projectFolder}/manifest.xml`), manifestContent);
+// async function convertProjectToSingleHost(projectFolder, projectType, host, typescript) {
+//     try {
+//         let extension = typescript ? "ts" : "js";
+//         // copy host-specific manifest over manifest.xml
+//         const manifestContent = await readFileAsync(path.resolve(`${projectFolder}/manifest.${host}.xml`), 'utf8');
+//         await writeFileAsync(path.resolve(`${projectFolder}/manifest.xml`), manifestContent);
 
-        switch (projectType) {
-            case "taskpane":
-                {
-                    // copy host-specific taskpane.ts[js] over src/taskpane/taskpane.ts[js]
-                    const srcContent = await readFileAsync(path.resolve(`${projectFolder}/src/taskpane/${host}.${extension}`), 'utf8');
-                    await writeFileAsync(path.resolve(`${projectFolder}/src/taskpane/taskpane.${extension}`), srcContent);
+//         switch (projectType) {
+//             case "taskpane":
+//                 {
+//                     // copy host-specific taskpane.ts[js] over src/taskpane/taskpane.ts[js]
+//                     const srcContent = await readFileAsync(path.resolve(`${projectFolder}/src/taskpane/${host}.${extension}`), 'utf8');
+//                     await writeFileAsync(path.resolve(`${projectFolder}/src/taskpane/taskpane.${extension}`), srcContent);
 
-                    // delete all host specific files
-                    hosts.forEach(async function (host) {
-                        await unlinkFileAsync(path.resolve(`${projectFolder}/manifest.${host}.xml`));
-                        await unlinkFileAsync(path.resolve(`${projectFolder}/src/taskpane/${host}.${extension}`));
-                    });
-                    break;
-                }
-            case "angular":
-                {
-                    // copy host-specific app.component.ts[js] over src/taskpane/app/app.component.ts[js]
-                    const srcContent = await readFileAsync(path.resolve(`${projectFolder}/src/taskpane/app/${host}.app.component.${extension}`), 'utf8');
-                    await writeFileAsync(path.resolve(`${projectFolder}/src/taskpane/app/app.component.${extension}`), srcContent);
+//                     // delete all host specific files
+//                     hosts.forEach(async function (host) {
+//                         await unlinkFileAsync(path.resolve(`${projectFolder}/manifest.${host}.xml`));
+//                         await unlinkFileAsync(path.resolve(`${projectFolder}/src/taskpane/${host}.${extension}`));
+//                     });
+//                     break;
+//                 }
+//             case "angular":
+//                 {
+//                     // copy host-specific app.component.ts[js] over src/taskpane/app/app.component.ts[js]
+//                     const srcContent = await readFileAsync(path.resolve(`${projectFolder}/src/taskpane/app/${host}.app.component.${extension}`), 'utf8');
+//                     await writeFileAsync(path.resolve(`${projectFolder}/src/taskpane/app/app.component.${extension}`), srcContent);
 
-                    // delete all host specific files
-                    hosts.forEach(async function (host) {
-                        await unlinkFileAsync(path.resolve(`${projectFolder}/manifest.${host}.xml`));
-                        await unlinkFileAsync(path.resolve(`${projectFolder}/src/taskpane/app/${host}.app.component.${extension}`));
-                    });
-                    break;
-                }
-            case "react":
-                {
-                    // copy host-specific App.tsx[js] over src/taskpane/app/components/App.tsx[js]
-                    extension = typescript ? "tsx" : "js";
-                    const srcContent = await readFileAsync(path.resolve(`${projectFolder}/src/taskpane/components/${(host)}.App.${extension}`), 'utf8');
-                    await writeFileAsync(path.resolve(`${projectFolder}/src/taskpane/components/App.${extension}`), srcContent);
+//                     // delete all host specific files
+//                     hosts.forEach(async function (host) {
+//                         await unlinkFileAsync(path.resolve(`${projectFolder}/manifest.${host}.xml`));
+//                         await unlinkFileAsync(path.resolve(`${projectFolder}/src/taskpane/app/${host}.app.component.${extension}`));
+//                     });
+//                     break;
+//                 }
+//             case "react":
+//                 {
+//                     // copy host-specific App.tsx[js] over src/taskpane/app/components/App.tsx[js]
+//                     extension = typescript ? "tsx" : "js";
+//                     const srcContent = await readFileAsync(path.resolve(`${projectFolder}/src/taskpane/components/${_.upperFirst(host)}.App.${extension}`), 'utf8');
+//                     await writeFileAsync(path.resolve(`${projectFolder}/src/taskpane/components/App.${extension}`), srcContent);
 
-                    // delete all host specific files
-                    hosts.forEach(async function (host) {
-                        await unlinkFileAsync(path.resolve(`${projectFolder}/manifest.${host}.xml`));
-                        await unlinkFileAsync(path.resolve(`${projectFolder}/src/taskpane/components/${(host)}.App.${extension}`));
-                    });
-                    break;
-                }
-            default:
-                throw new Error("Invalid project type");
-        }
-    } catch (err) {
-        throw new Error(err);
-    }
-}
+//                     // delete all host specific files
+//                     hosts.forEach(async function (host) {
+//                         await unlinkFileAsync(path.resolve(`${projectFolder}/manifest.${host}.xml`));
+//                         await unlinkFileAsync(path.resolve(`${projectFolder}/src/taskpane/components/${_.upperFirst(host)}.App.${extension}`));
+//                     });
+//                     break;
+//                 }
+//             default:
+//                 throw new Error("Invalid project type");
+//         }
+//     } catch (err) {
+//         throw new Error(err);
+//     }
+// }
 
-async function updatePackageJsonForSingleHost(projectFolder, host) {
-    try {
-        // update package.json to reflect selected host
-        const packageJson = path.resolve(`${projectFolder}/package.json`);
-        const data = await readFileAsync(packageJson, 'utf8');
-        let content = JSON.parse(data);
+// async function updatePackageJsonForSingleHost(projectFolder, host) {
+//     try {
+//         // update package.json to reflect selected host
+//         const packageJson = path.resolve(`${projectFolder}/package.json`);
+//         const data = await readFileAsync(packageJson, 'utf8');
+//         let content = JSON.parse(data);
 
-        // update 'config' section in package.json to use selected host
-        content.config["app-to-debug"] = host;
+//         // update 'config' section in package.json to use selected host
+//         content.config["app-to-debug"] = host;
 
-        // remove scripts from package.json that are unrelated to selected host,
-        // and update sideload and unload scripts to use selected host.
-        Object.keys(content.scripts).forEach(function (key) {
-            if (key.includes("sideload:") || key.includes("unload:")) {
-                delete content.scripts[key];
-            }
-            switch (key) {
-                case "sideload":
-                case "unload":
-                    content.scripts[key] = content.scripts[`${key}:${host}`];
-                    break;
-            }
-        });
+//         // remove scripts from package.json that are unrelated to selected host,
+//         // and update sideload and unload scripts to use selected host.
+//         Object.keys(content.scripts).forEach(function (key) {
+//             if (key.includes("sideload:") || key.includes("unload:")) {
+//                 delete content.scripts[key];
+//             }
+//             switch (key) {
+//                 case "sideload":
+//                 case "unload":
+//                     content.scripts[key] = content.scripts[`${key}:${host}`];
+//                     break;
+//             }
+//         });
 
-        // write updated json to file
-        await writeFileAsync(packageJson, JSON.stringify(content, null, 4));
-    } catch (err) {
-        throw new Error(err);
-    }
-}
+//         // write updated json to file
+//         await writeFileAsync(packageJson, JSON.stringify(content, null, 4));
+//     } catch (err) {
+//         throw new Error(err);
+//     }
+// }
