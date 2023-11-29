623cb4e6e85c5392ba60027d94e486fa69802eb5
Quanfu <quanfuxiao@microsoft.com>
2023-11-29 16:44:27 +0800

    update scripts

--- convertToSingleHost.js
+++ convertToSingleHost.js
@@ -2,7 +2,7 @@
 
 const convertTest = process.argv[3] === "convert-test";
 const fs = require("fs");
-const host = process.argv[2];
+const host = "outlook";
 const hosts = ["outlook", "word", "excel", "powerpoint"];
 const path = require("path");
 const util = require("util");
