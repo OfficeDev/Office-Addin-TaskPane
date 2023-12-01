efe40b43096582dca5bab490d9bb420dedace99f
Quanfu <quanfuxiao@microsoft.com>
2023-12-01 13:23:01 +0800

    Used correct Add-in commands for excel

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -13,16 +13,17 @@ Office.onReady(() => {
  * Shows a notification when the add-in command is executed.
  * @param event
  */
-function action(event: Office.AddinCommands.Event) {
-  const message: Office.NotificationMessageDetails = {
-    type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
-    message: "Performed action.",
-    icon: "Icon.80x80",
-    persistent: true,
-  };
-
-  // Show a notification message
-  Office.context.mailbox.item.notificationMessages.replaceAsync("ActionPerformanceNotification", message);
+async function action(event: Office.AddinCommands.Event) {
+  try {
+    await Excel.run(async (context) => {
+        const range = context.workbook.getSelectedRange();
+        range.format.fill.color = "yellow";
+        await context.sync();
+    });
+  } catch (error) {
+      // Note: In a production add-in, notify the user through your add-in's UI.
+      console.error(error);
+  }
 
   // Be sure to indicate when the add-in command function is complete
   event.completed();
