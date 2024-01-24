50888be4a8591e226b5e0c658e160b17ecb7d39d
YueLi <yueli2@microsoft.com>
2024-01-24 17:54:42 +0800

    test log

--- convertToSingleHost.js
+++ convertToSingleHost.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const util = require("util");
 const childProcess = require("child_process");
+const { error } = require("console");
 
 const host = process.argv[2];
 const manifestType = process.argv[3];
@@ -33,6 +34,7 @@ async function modifyProjectForSingleHost(host) {
   }
   if (manifestType === "json") {
     await convertProjectToSingleHostForJsonManifest(host);
+    throw new error("JSON manifest is not supported yet.");
   } else {
     await convertProjectToSingleHostForXMLManifest(host);
   }
