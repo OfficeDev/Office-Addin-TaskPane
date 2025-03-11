a1d2a83a46a649545d579c3c8ebd86a71d2c0e25
Darren Miller <darrmill@microsoft.com>
2025-03-11 11:58:25 -0700

    Fix Outlook event code

--- src/events/events.ts
+++ src/events/events.ts
@@ -1,8 +1,8 @@
 import { runOutlook } from "../shared/outlook";
 
-export function onMessageCompose(event: Office.MailboxEvent) {
+export function onMessageComposeHandler(event: Office.MailboxEvent) {
   runOutlook("Message compose event");
   event.completed();
 }
 
-Office.actions.associate("onMsgCompose", onMessageCompose);
+Office.actions.associate("onMessageComposeHandler", onMessageComposeHandler);
