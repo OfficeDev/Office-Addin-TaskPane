d7f24fe5aa86497431b294e5b735d8d67231505f
Darren Miller <darrmill@microsoft.com>
2025-10-28 11:17:46 -0700

    Fix linter error

--- src/taskpane/powerpoint.ts
+++ src/taskpane/powerpoint.ts
@@ -3,7 +3,7 @@
  * See LICENSE in the project root for license information.
  */
 
-/* global document, PowerPoint */
+/* global document, Office, PowerPoint */
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.PowerPoint) {
